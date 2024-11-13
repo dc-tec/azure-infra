d46562a711ae733cd22749afe90e7d524c4f7c27
Roel de Cort <roel@decort.tech>
2024-11-13 01:01:03 +0100

    create unique id for each app role and update backend and version settings

--- src/entra/applications/locals.tf
+++ src/entra/applications/locals.tf
@@ -3,4 +3,13 @@ locals {
     for application in yamldecode(file("${path.module}/_configuration/applications.yaml")).applications :
     application.name => application...
   }
+
+  app_roles = merge(
+    [
+      for _, application in local.applications : {
+        for _, app_role in application[0].app_roles :
+        "${application[0].name}-${app_role.name}" => app_role
+      }
+    ]...
+  )
 }
